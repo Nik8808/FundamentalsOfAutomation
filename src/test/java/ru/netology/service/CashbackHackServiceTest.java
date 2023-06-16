3762a959e2764c3395f168cbe98964739a8ebaa2
Nikita <inik8808@gmail.com>
2023-06-16 12:28:25 +0500

    Created by junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,24 +1,21 @@
 package ru.netology.service;
-
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void correctAccrual() {
-        assertEquals(service.remain(1001), 999);
+        assertEquals(999, service.remain(1001));
     }
 
     @Test
     public void shouldAmountZero() {
-        assertEquals(service.remain(1000), 0);
+        assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void purchaseAnAmountUpTo1000() {
-        assertEquals(service.remain(999), 1);
+        assertEquals(1, service.remain(999));
     }
 }
